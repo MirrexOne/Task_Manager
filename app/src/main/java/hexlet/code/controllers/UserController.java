cd43a4718906f613c83e74a0a97467ec7306f11c
Alexander Belovitsky <mirrexone@gmail.com>
2024-10-13 17:50:53 +0300

    Fix project

--- app/src/main/java/hexlet/code/controllers/UserController.java
+++ app/src/main/java/hexlet/code/controllers/UserController.java
@@ -7,7 +7,13 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -30,7 +36,9 @@ public class UserController {
 
     @PutMapping("/{id}")
     @PreAuthorize("authentication.principal.username == @userServiceImpl.getUserById(#id).email")
-    public ResponseEntity<UserResponse> updateUser(@PathVariable Long id, @Valid @RequestBody UpdateUserRequest updateUserRequest) {
+    public ResponseEntity<UserResponse> updateUser(
+            @PathVariable Long id,
+            @Valid @RequestBody UpdateUserRequest updateUserRequest) {
         return ResponseEntity.ok(userService.updateUser(id, updateUserRequest));
     }
 
